a0f0ed2c34d3a3eb6af41ca9a48423e351d3c914
Laseduh <ekidiga6@gmail.com>
2022-06-05 13:36:38 -0700

    Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,22 +8,17 @@
  */
 int main(void)
 {
-	char d = '0';
+	int n;
+	char ch;
 
-	while (d <= '9')
+	for (n = 48; n < 58; n++)
 	{
-		putchar(d);
-		d++;
+		putchar(n);
 	}
-
-	char c = 'a';
-
-	while (c <= 'f')
+	for (ch = 'a'; ch <= 'f'; ch++)
 	{
-		putchar(c);
-		c++;
+		putchar(ch);
 	}
-
 	putchar('\n');
 	return (0);
 }
