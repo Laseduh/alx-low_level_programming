965afd74c482842a0dbf94c2d3173540e0772662
Laseduh <ekidiga6@gmail.com>
2022-06-05 12:13:05 -0700

    Combinations three digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -16,12 +16,10 @@ int main(void)
 			{
 				putchar((a / 10) + '0');
 				putchar((a % 10) + '0');
-				putchar(' ');
 				putchar((b / 10) + '0');
 				putchar((b % 10) + '0');
 				if (!(a == 98 && b == 99))
 				{
-					putchar(',');
 					putchar(',');
 				}
 			}
@@ -29,4 +27,4 @@ int main(void)
 	}
 	putchar('\n');
 	return (0);
-}
+}i
