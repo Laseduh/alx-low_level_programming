158a5f23c55231c728ea5c7bc3786a2d4bb18d6f
Laseduh <ekidiga6@gmail.com>
2022-06-08 02:31:41 -0700

    9-times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -19,9 +19,9 @@ void times_table(void)
 			{
 				_putchar(',');
 				_putchar(' ');
+				_putchar(' ');
 				_putchar(k + '0');
-			}
-			else if (k >= 10)
+			} else if (k >= 10)
 			{
 				_putchar(',');
 				_putchar(' ');
