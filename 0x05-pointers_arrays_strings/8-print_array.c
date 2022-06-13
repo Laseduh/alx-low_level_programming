312941f38daf4e1e0e11e3aa3cc5a7275549d1da
Laseduh <ekidiga6@gmail.com>
2022-06-13 14:21:04 -0700

    Arrays not pointers

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include "main.h"
+
+/**
+ * print_array - update value.
+ * @a: value to be evaluate.
+ * @n: value to be evaluate.
+ * Return: not.
+ */
+void print_array(int *a, int n)
+{
+	int x;
+
+	for (x = 0 ; x < n; x++)
+	{
+		printf("%d", a[x]);
+		printf("%d", a[x]);
+		{
+			printf("%d", a[x]);
+		}
+	}
+	printf("\n");
+}
