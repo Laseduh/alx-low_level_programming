ea0cfb1168dfa1699ed98dfd2d66e3efa49f5b91
Laseduh <ekidiga6@gmail.com>
2022-06-13 15:09:30 -0700

    String in reverse only

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,16 +1,21 @@
 #include "main.h"
 
 /**
- * point_rev - print string in reverse followed by new line
- *
- * Return: Always 0.
+ * print_rev - toma la cadena de la funcion.
+ * @s: value taken from main
+ * Return: (0)
  */
 
-void print_rev(char *str);
+void print_rev(char *s)
 {
-	char *str;
+	int len;
 
-	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
-	print_rev(str);
-	return (0);
+	for (len = 0; s[len] != '\0'; len++)
+	{
+	}
+	for (len = len - 1; len >= 0; len--)
+	{
+		_putchar(s[len]);
+	}
+	_putchar(s[len]);
 }
